bcd875054d9383d4877940f18047f0770dbb8e2c
EC2 Default User <ec2-user@ip-172-31-27-92.us-east-2.compute.internal>
2020-03-02 07:02:03 +0000

    AutomatedCommitAndPush

--- js/app.js
+++ js/app.js
@@ -32,17 +32,16 @@ $("#searchButton").on("click", function() {
             var img3 = result.hits[picIndex3].webformatURL;
             var img4 = result.hits[picIndex4].webformatURL;
             
-            console.log("urls");
-            console.log(img1);
-            console.log(img2);
-            console.log(img3);
-            console.log(img4);
-            console.log("end urls");
             
             $("#img1").attr("src",img1);
             $("#img2").attr("src",img2);
             $("#img3").attr("src",img3);
             $("#img4").attr("src",img4);
+            
+            $("#likes1").html("likes: " + result.hits[picIndex1].likes)
+            $("#likes2").html("likes: " + result.hits[picIndex2].likes)
+            $("#likes3").html("likes: " + result.hits[picIndex3].likes)
+            $("#likes4").html("likes: " + result.hits[picIndex4].likes)
             
             console.log(result);
         },
